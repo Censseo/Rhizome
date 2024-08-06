71d4e291f1875e2b90f7180d9c2da404034d5d53
Hara Peko <harapeko.suiren@gmail.com>
2024-08-06 09:46:50 +0000

    Remove non compiling code

--- app-dnsseeder/src/main/java/rhizome/config/Api.java
+++ app-dnsseeder/src/main/java/rhizome/config/Api.java
@@ -3,17 +3,20 @@ package rhizome.config;
 import java.util.concurrent.Executor;
 
 import io.activej.eventloop.Eventloop;
-import io.activej.http.AsyncHttpServer;
 import io.activej.http.AsyncServlet;
 import io.activej.http.HttpResponse;
+import io.activej.http.HttpServer;
 import io.activej.http.RoutingServlet;
 import io.activej.http.StaticServlet;
+import io.activej.http.loader.IStaticLoader;
 import io.activej.inject.annotation.Eager;
 import io.activej.inject.annotation.Provides;
 import io.activej.inject.module.AbstractModule;
+import io.activej.reactor.Reactor;
 import lombok.extern.slf4j.Slf4j;
 
 import static io.activej.http.HttpMethod.*;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
 
 @Slf4j
 public class Api extends AbstractModule {
@@ -25,29 +28,43 @@ public class Api extends AbstractModule {
         return new Api();
     }
 
-    @Provides
-    AsyncServlet servlet(Executor executor) {
-        log.info("HTTP Server is now available at http://localhost:" + PORT);
-        return RoutingServlet.create()
-            .map(POST, "/hello", request -> request.loadBody()
-                .map($ -> {
-                    String name = request.getPostParameters().get("name");
-                    return HttpResponse.ok200()
-                        .withHtml("<h1><center>Hello from POST, " + name + "!</center></h1>");
-                }))
-            .map(GET, "/hello", request -> {
-                String name = request.getQueryParameter("name");
-                return HttpResponse.ok200()
-                    .withHtml("<h1><center>Hello from GET, " + name + "!</center></h1>");
-            })
-            .map("/*", StaticServlet.ofClassPath(executor, RESOURCE_DIR)
-                .withIndexHtml());
-    }
+	@Provides
+	Executor executor() {
+		return newSingleThreadExecutor();
+	}
+
+	//[START REGION_1]
+	@Provides
+	IStaticLoader staticLoader(Reactor reactor, Executor executor) {
+		return IStaticLoader.ofClassPath(reactor, executor, RESOURCE_DIR);
+	}
+
+	@Provides
+	AsyncServlet servlet(Reactor reactor, IStaticLoader staticLoader) {
+		return RoutingServlet.builder(reactor)
+			.with(POST, "/hello", request -> request.loadBody()
+				.then($ -> {
+					String name = request.getPostParameters().get("name");
+					return HttpResponse.ok200()
+						.withHtml("<h1><center>Hello from POST, " + name + "!</center></h1>")
+						.toPromise();
+				}))
+			.with(GET, "/hello", request -> {
+				String name = request.getQueryParameter("name");
+				return HttpResponse.ok200()
+					.withHtml("<h1><center>Hello from GET, " + name + "!</center></h1>")
+					.toPromise();
+			})
+			.with("/*", StaticServlet.builder(reactor, staticLoader)
+				.withIndexHtml()
+				.build())
+			.build();
+	}
 
     @Provides
     @Eager
-    AsyncHttpServer server(Eventloop eventloop, AsyncServlet servlet) {
-        return AsyncHttpServer.create(eventloop, servlet).withListenPort(PORT);
+    HttpServer server(Eventloop eventloop, AsyncServlet servlet) {
+        return HttpServer.builder(eventloop, servlet).withListenPort(PORT).build();
     }
     
 }
--- app-dnsseeder/src/main/java/rhizome/services/blockchain/BlockchainService.java
+++ app-dnsseeder/src/main/java/rhizome/services/blockchain/BlockchainService.java
@@ -1,14 +1,14 @@
-package rhizome.services.blockchain;
+// package rhizome.services.blockchain;
 
-import lombok.Getter;
-import lombok.Setter;
-import rhizome.core.blockchain.AbstractBlockchain;
-import io.activej.reactor.Reactor;
+// import lombok.Getter;
+// import lombok.Setter;
+// import rhizome.core.blockchain.AbstractBlockchain;
+// import io.activej.reactor.Reactor;
 
-@Getter @Setter
-public class BlockchainService extends AbstractBlockchain {
+// @Getter @Setter
+// public class BlockchainService extends AbstractBlockchain {
 
-    public BlockchainService(Reactor reactor) {
-        super(reactor);
-    }   
-}
+//     public BlockchainService(Reactor reactor) {
+//         super(reactor);
+//     }   
+// }
